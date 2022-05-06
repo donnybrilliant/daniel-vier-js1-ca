8389a952d11f76565b44d1f3b4a9a48af98dbc4b
donnybrilliant <daniel.vier@gmail.com>
2022-05-06 17:59:32 +0200

    Add queryString

--- js/details.js
+++ js/details.js
@@ -2,7 +2,11 @@ const container = document.querySelector("main");
 
 const apiKey = "LBY1JKNzQTx6Cl12s9d8wAEp4ro2dH80";
 
-const url = `https://app.ticketmaster.com/discovery/v2/events/Z698xZb_Z17QA7y?apikey=${apiKey}&locale=*`;
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const eventId = params.get("id");
+
+const url = `https://app.ticketmaster.com/discovery/v2/events/${eventId}?apikey=${apiKey}&locale=*`;
 
 async function fetchEvent() {
   try {
@@ -10,9 +14,15 @@ async function fetchEvent() {
     const event = await response.json();
     container.innerHTML = "";
     console.log(event);
-
-    container.innerHTML += `
-      <h1>${event.name}</h1>`;
+    const date = event.dates.start.localDate;
+    const [year, month, day] = date.split("-");
+    const formattedDate = [month, day, year].join("/");
+    container.innerHTML += `<div class="event">
+      <h1>${event.name}</h1>
+      <p>${formattedDate}</p>
+      <img src="${event.images[2].url}">
+      <div class="details">
+      <a href="${event.url}" target="_blank" class="tickets">Billetter</a></div></div>`;
   } catch (error) {
     console.log(error);
   }
